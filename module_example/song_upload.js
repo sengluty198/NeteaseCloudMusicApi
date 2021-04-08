e3e8fd925225698a89601e0f6a77db94f1534207
sengluty198 <30923741+sengluty198@users.noreply.github.com>
2021-04-08 23:08:37 +0800

    Update song_upload.js

--- module_example/song_upload.js
+++ module_example/song_upload.js
@@ -4,8 +4,8 @@ const path = require('path')
 
 async function main() {
   const result = await login_cellphone({
-    phone: '手机号',
-    password: '密码',
+    phone: 18873884507,
+    password: ,
   })
   const filePath = './test.mp3'
   try {
